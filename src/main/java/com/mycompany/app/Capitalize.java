00a8b0dbfe662a806e690c76d9e55f8c0bb800c2
DelphicGen <gennardokidam@gmail.com>
2021-01-22 16:05:35 +0700

    Initialize

---
+++ src/main/java/com/mycompany/app/Capitalize.java
@@ -0,0 +1,24 @@
+package com.mycompany.app;
+
+public class Capitalize {
+
+  //
+  public String capitalizeWithoutLib(){
+
+  }
+
+  //
+  public String splitWithoutLib(){
+
+  }
+
+  //
+  public String splitWithLib(){
+
+  }
+
+  //
+  public String capitalizeWithLib(){
+
+  }
+}
