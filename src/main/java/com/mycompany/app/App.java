e30d1fb3fbf31b9c2638f227d0d740ac9d4af52f
kevincahyadigiri <kevincahyadigiri@gmail.com>
2021-01-22 16:56:56 +0700

    add method capitalize without lib, split without lib

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,10 +10,15 @@ public class App
 
     public static void main( String[] args )
     {
-        // Scanner sc = new Scanner(System.in);
-        // System.out.print("Tulis suatu kalimat: ");
-        // String sentence = sc.nextLine();
-        // sc.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tulis suatu kalimat: ");
+        String sentence = sc.nextLine();
+        sc.close();
+        Capitalize capt = new Capitalize(sentence);
+        String stc = capt.capitalizeWithoutLib();
+        System.out.println(stc);
+
+
         // System.out.println(sentence);
         // System.out.println( "Hello World!" );
 
